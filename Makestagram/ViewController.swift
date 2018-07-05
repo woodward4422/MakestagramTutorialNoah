87277668f07b925cef83dde5c3db02a532161786
noah <noahwoodward2244@gmail.com>
2018-07-05 09:47:31 -0700

    Initial Commit

---
+++ Makestagram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Makestagram
+//
+//  Created by Noah Woodward on 7/5/18.
+//  Copyright © 2018 Noah Woodward. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
